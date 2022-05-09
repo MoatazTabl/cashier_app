a2eba76a7a26fae402bc3af32376e533f802a8ab
moata <moata@MOATAZ>
2022-05-09 23:53:13 +0200

    Edited the design in cashier form

--- WinFormsApp6/cashier.Designer.vb
+++ WinFormsApp6/cashier.Designer.vb
@@ -26,19 +26,18 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Id = New System.Windows.Forms.TextBox()
+        Me.quantity = New System.Windows.Forms.TextBox()
+        Me.Price = New System.Windows.Forms.TextBox()
+        Me.Name = New System.Windows.Forms.TextBox()
+        Me.selling_price = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.product_list = New System.Windows.Forms.ListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -49,6 +48,7 @@ Partial Class Form1
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(136, 72)
         Me.Button1.TabIndex = 0
+        Me.Button1.TabStop = False
         Me.Button1.Text = "حذف"
         Me.Button1.UseVisualStyleBackColor = False
         '
@@ -60,6 +60,7 @@ Partial Class Form1
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(136, 72)
         Me.Button2.TabIndex = 1
+        Me.Button2.TabStop = False
         Me.Button2.Text = "اضافة"
         Me.Button2.UseVisualStyleBackColor = False
         '
@@ -71,6 +72,7 @@ Partial Class Form1
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(136, 72)
         Me.Button4.TabIndex = 3
+        Me.Button4.TabStop = False
         Me.Button4.Text = "عرض الصنف"
         Me.Button4.UseVisualStyleBackColor = False
         '
@@ -82,62 +84,55 @@ Partial Class Form1
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(136, 72)
         Me.Button5.TabIndex = 4
+        Me.Button5.TabStop = False
         Me.Button5.Text = "تعديل"
         Me.Button5.UseVisualStyleBackColor = False
         '
-        'TextBox1
+        'Id
         '
-        Me.TextBox1.BackColor = System.Drawing.Color.LavenderBlush
-        Me.TextBox1.Location = New System.Drawing.Point(130, 54)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(183, 20)
-        Me.TextBox1.TabIndex = 5
+        Me.Id.BackColor = System.Drawing.Color.LavenderBlush
+        Me.Id.Location = New System.Drawing.Point(130, 54)
+        Me.Id.Name = "Id"
+        Me.Id.Size = New System.Drawing.Size(183, 20)
+        Me.Id.TabIndex = 0
         '
-        'TextBox2
+        'quantity
         '
-        Me.TextBox2.BackColor = System.Drawing.Color.LavenderBlush
-        Me.TextBox2.Location = New System.Drawing.Point(130, 115)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(183, 20)
-        Me.TextBox2.TabIndex = 6
+        Me.quantity.BackColor = System.Drawing.Color.LavenderBlush
+        Me.quantity.Location = New System.Drawing.Point(130, 115)
+        Me.quantity.Name = "quantity"
+        Me.quantity.Size = New System.Drawing.Size(183, 20)
+        Me.quantity.TabIndex = 2
         '
-        'TextBox3
+        'Price
         '
-        Me.TextBox3.BackColor = System.Drawing.Color.LavenderBlush
-        Me.TextBox3.Location = New System.Drawing.Point(130, 170)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(183, 20)
-        Me.TextBox3.TabIndex = 7
+        Me.Price.BackColor = System.Drawing.Color.LavenderBlush
+        Me.Price.Location = New System.Drawing.Point(130, 170)
+        Me.Price.Name = "Price"
+        Me.Price.Size = New System.Drawing.Size(183, 20)
+        Me.Price.TabIndex = 4
         '
-        'TextBox4
+        'Name
         '
-        Me.TextBox4.BackColor = System.Drawing.Color.LavenderBlush
-        Me.TextBox4.Location = New System.Drawing.Point(562, 54)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(183, 20)
-        Me.TextBox4.TabIndex = 8
+        Me.Name.BackColor = System.Drawing.Color.LavenderBlush
+        Me.Name.Location = New System.Drawing.Point(562, 54)
+        Me.Name.Name = "Name"
+        Me.Name.Size = New System.Drawing.Size(183, 20)
+        Me.Name.TabIndex = 1
         '
-        'TextBox5
+        'selling_price
         '
-        Me.TextBox5.BackColor = System.Drawing.Color.LavenderBlush
-        Me.TextBox5.Location = New System.Drawing.Point(562, 115)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(183, 20)
-        Me.TextBox5.TabIndex = 9
-        '
-        'TextBox6
-        '
-        Me.TextBox6.BackColor = System.Drawing.Color.LavenderBlush
-        Me.TextBox6.Location = New System.Drawing.Point(562, 176)
-        Me.TextBox6.Name = "TextBox6"
-        Me.TextBox6.Size = New System.Drawing.Size(183, 20)
-        Me.TextBox6.TabIndex = 10
+        Me.selling_price.BackColor = System.Drawing.Color.LavenderBlush
+        Me.selling_price.Location = New System.Drawing.Point(562, 115)
+        Me.selling_price.Name = "selling_price"
+        Me.selling_price.Size = New System.Drawing.Size(183, 20)
+        Me.selling_price.TabIndex = 5
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Arabic Typesetting", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(31, 52)
+        Me.Label1.Location = New System.Drawing.Point(47, 52)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(68, 26)
         Me.Label1.TabIndex = 11
@@ -147,7 +142,7 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Arabic Typesetting", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(31, 109)
+        Me.Label2.Location = New System.Drawing.Point(56, 113)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(48, 26)
         Me.Label2.TabIndex = 12
@@ -157,55 +152,43 @@ Partial Class Form1
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Arabic Typesetting", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(31, 164)
+        Me.Label3.Location = New System.Drawing.Point(62, 168)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(65, 26)
+        Me.Label3.Size = New System.Drawing.Size(42, 26)
         Me.Label3.TabIndex = 13
-        Me.Label3.Text = "سعر الجملة"
+        Me.Label3.Text = "السعر"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Arabic Typesetting", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label4.Location = New System.Drawing.Point(450, 54)
+        Me.Label4.Location = New System.Drawing.Point(476, 52)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(72, 26)
         Me.Label4.TabIndex = 14
         Me.Label4.Text = "اسم الصنف"
         Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Arabic Typesetting", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label5.Location = New System.Drawing.Point(450, 115)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(49, 26)
-        Me.Label5.TabIndex = 15
-        Me.Label5.Text = "المخزون"
-        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
         'Label6
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Arabic Typesetting", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label6.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label6.Location = New System.Drawing.Point(450, 170)
+        Me.Label6.Location = New System.Drawing.Point(476, 113)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(61, 26)
         Me.Label6.TabIndex = 16
         Me.Label6.Text = "سعر البيع"
         '
-        'product_list
+        'DataGridView1
         '
-        Me.product_list.BackColor = System.Drawing.Color.LavenderBlush
-        Me.product_list.FormattingEnabled = True
-        Me.product_list.Location = New System.Drawing.Point(47, 214)
-        Me.product_list.Name = "product_list"
-        Me.product_list.Size = New System.Drawing.Size(810, 225)
-        Me.product_list.TabIndex = 17
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(47, 224)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(810, 189)
+        Me.DataGridView1.TabIndex = 17
         '
         'Form1
         '
@@ -213,19 +196,17 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
         Me.ClientSize = New System.Drawing.Size(884, 561)
-        Me.Controls.Add(Me.product_list)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox6)
-        Me.Controls.Add(Me.TextBox5)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.selling_price)
+        Me.Controls.Add(Me.Name)
+        Me.Controls.Add(Me.Price)
+        Me.Controls.Add(Me.quantity)
+        Me.Controls.Add(Me.Id)
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button2)
@@ -234,6 +215,7 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Cashier"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -243,17 +225,15 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents Button5 As Button
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Id As TextBox
+    Friend WithEvents quantity As TextBox
+    Friend WithEvents Price As TextBox
+    Friend WithEvents Name As TextBox
+    Friend WithEvents selling_price As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents product_list As ListBox
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- WinFormsApp6/cashier.vb
+++ WinFormsApp6/cashier.vb
@@ -1,18 +1,18 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        TextBox1.Clear()
-        TextBox2.Clear()
-        TextBox3.Clear()
-        TextBox4.Clear()
+        Id.Clear()
+        quantity.Clear()
+        Price.Clear()
+        Name.Clear()
         TextBox5.Clear()
-        TextBox6.Clear()
+        selling_price.Clear()
     End Sub
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles product_list.SelectedIndexChanged
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles Id.TextChanged
 
     End Sub
 
@@ -21,6 +21,10 @@
     End Sub
 
     Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
 End Class
