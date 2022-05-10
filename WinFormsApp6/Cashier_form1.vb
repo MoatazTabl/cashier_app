9e3d889de292d2dec398faf965767b8fcbd27145
moata <moata@MOATAZ>
2022-05-10 03:56:35 +0200

    Finished cashier form finally

--- WinFormsApp6/Cashier_form1.vb
+++ WinFormsApp6/Cashier_form1.vb
@@ -6,6 +6,14 @@ Public Class Cashierform
     Dim reader As SqlDataReader
 
     Dim ds As DataSet
+
+    Public Function Clear_1()
+        Id.Clear()
+        text_box.Clear()
+        quantity.Clear()
+        Price.Clear()
+        selling_price.Clear()
+    End Function
     Private Sub Cashier_form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         conn = New SqlConnection("Data Source=localhost;Initial Catalog=MedicalHouse;Integrated Security=True")
         conn.Open()
@@ -26,6 +34,9 @@ Public Class Cashierform
         dr("selling price") = Val(selling_price.Text)
         ds.Tables("products1").Rows.Add(dr)
         da.Update(ds, "products1")
+        MessageBox.Show("Done", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Clear_1()
+
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles disply_button.Click
@@ -38,6 +49,7 @@ Public Class Cashierform
                 quantity.Text = reader(2)
                 Price.Text = reader(3)
                 selling_price.Text = reader(4)
+                conn.Close()
             Else
                 DataGridView1.DataBindings.Add("datasource", ds, "products1")
             End If
@@ -45,5 +57,42 @@ Public Class Cashierform
             MessageBox.Show(ex.Message)
         End Try
 
+    End Sub
+
+    Private Sub delete_button_Click(sender As Object, e As EventArgs) Handles delete_button.Click
+        Dim answer As Integer
+        Dim dr As DataRow = Nothing
+        For Each dr In ds.Tables("products1").Rows
+            If dr("id") = Val(Id.Text) Then
+                text_box.Text = dr("name")
+                quantity.Text = dr("quantity")
+                Price.Text = dr("price")
+                selling_price.Text = dr("selling price")
+                Exit For
+            End If
+        Next
+        answer = MessageBox.Show(
+            "are you sure you want to delete this record?", "Warrnning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning)
+        If answer = System.Windows.Forms.DialogResult.No Then Exit Sub
+        dr.Delete()
+        da.Update(ds, "products1")
+
+    End Sub
+
+    Private Sub edit_button_Click(sender As Object, e As EventArgs) Handles edit_button.Click
+        Dim dr As DataRow = Nothing
+        For Each dr In ds.Tables("products1").Rows
+            If dr("id") = Val(Id.Text) Then
+                dr("name") = text_box.Text
+                dr("quantity") = Val(quantity.Text)
+                dr("price") = Val(Price.Text)
+                dr("selling price") = Val(selling_price.Text)
+                Exit For
+            End If
+        Next
+        MessageBox.Show("Done", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Clear_1()
+
+        da.Update(ds, "products1")
     End Sub
 End Class
